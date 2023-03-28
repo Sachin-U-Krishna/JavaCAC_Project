d637ef55a07a8db67294ad603de91a1e9de87634
Sachin Krishna <liberty.sachin56@gmail.com>
2023-03-28 11:37:29 +0530

    Loginstructure

--- src/Login.java
+++ src/Login.java
@@ -1,2 +1,27 @@
-public class Login {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Login extends JFrame{
+        private JTextField username;
+        private JTextField password;
+        private JButton login;
+    private JPanel panelMain;
+
+    public Login() {
+        login.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(login,username.getText()+" , Hello");
+            }
+        });
+    }
+    public static void main(String[] args) {
+        Login h= new Login();
+        h.setContentPane(h.panelMain);
+        h.setTitle("Hi");
+        h.setSize(500,400);
+        h.setVisible(true);
+        h.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
