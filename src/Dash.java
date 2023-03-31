ef6957a1855730fca5f863c7bdd8aa70fe526905
Yogisha <yogisha9g@gmail.com>
2023-03-31 10:54:30 +0530

    Dash

--- src/Dash.java
+++ src/Dash.java
@@ -1,10 +1,24 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Dash extends JFrame{
-    private JList list1;
-    private JTextField textField1;
-    private JTextField textField2;
     private JPanel panelDash;
+    private JPanel listPanel;
+    private JPanel parentPanel;
+    private JPanel homePanel;
+    private JButton homeButton;
+    private JButton profileButton;
+    private JButton ordersButton;
+    private JPanel profilePanel;
+    private JPanel Iphone;
+    private JPanel Tab;
+    private JPanel Laptop;
+    private JPanel Mobile;
+    private JPanel Watch;
+    private JPanel Earphone;
+    private JPanel orderPanel;
 
     private JFrame h = new JFrame("Dash");
     public Dash() {
@@ -14,6 +28,37 @@ public class Dash extends JFrame{
         h.setVisible(true);
         h.setResizable(false);
         h.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel greenPanel=new JPanel();
+        greenPanel.setBackground(Color.blue);
+        greenPanel.setBounds(250,0,250,250);
+        homeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                parentPanel.removeAll();
+                parentPanel.add(homePanel);
+                parentPanel.repaint();
+                parentPanel.revalidate();
+            }
+        });
+        profileButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                parentPanel.removeAll();
+                parentPanel.add(profilePanel);
+                parentPanel.repaint();
+                parentPanel.revalidate();
+            }
+        });
+        ordersButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                parentPanel.removeAll();
+                parentPanel.add(orderPanel);
+                parentPanel.repaint();
+                parentPanel.revalidate();
+            }
+        });
     }
 
     public static void main(String[] args) {
