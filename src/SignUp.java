0010058d5bf11cc80b4a8007023698c0036d7614
Yogisha <yogisha9g@gmail.com>
2023-04-01 22:08:31 +0530

    Try

--- src/SignUp.java
+++ src/SignUp.java
@@ -1,21 +1,26 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class SignUp extends JFrame  {
     private JPanel panelSignup;
-    private JTextField firstName;
+    private JTextField firstname;
     private JTextField email;
-    private JTextField createPassword;
+    private JTextField password;
     private JComboBox comboBox1;
-    private JFormattedTextField a91FormattedTextField;
-    private JTextField lastNameTextField;
+    private JFormattedTextField phone;
+    private JTextField lastname;
     private JButton submitSignUp;
     private JComboBox comboBox2;
     private JButton cancelButton;
 
     private JLabel GenderSelected;
     private JFrame frame = new JFrame("SignUp");
+    private String DB_PASS = "";
     public SignUp() {
         frame.setContentPane(panelSignup);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -34,7 +39,24 @@ public class SignUp extends JFrame  {
         submitSignUp.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                try{
+                    Class.forName("com.mysql.jdbc.Driver");
+                    Connection con= DriverManager.getConnection(
+                            "jdbc:mysql://localhost:3306/JDBC","root",DB_PASS);
+                    Statement stmt = con.createStatement();
+                    String s = "INSERT into users(firstname,lastname,email,password,phone,gender,location) values('"+firstname.getText()+"','"+lastname.getText()+"','"+email.getText()+"','"+password.getText()+"','"+phone.getText()+"','"+comboBox1.getSelectedItem().toString()+"','"+comboBox2.getSelectedItem().toString()+"')";
+                    stmt.execute(s);
+                    JOptionPane.showMessageDialog(panelSignup, "User Registered");
+                    frame.dispose();
+                    new Login();
+                }catch(Exception err){ System.out.println(err);}
+            }
+        });
+        cancelButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+                new Login();
             }
         });
     }
